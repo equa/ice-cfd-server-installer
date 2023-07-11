1ec1d4aa50ad3e2874d7a7795e2148ab8e3514a6
Niklas Wikström <niklas.wikstrom@gmail.com>
2023-07-11 10:29:10 +0200

    ...

--- pack.sh
+++ pack.sh
@@ -2,4 +2,5 @@
 
 git branch --show-current > revision
 git rev-parse HEAD >> revision
+rm ice-cfd-server-installer.tgz
 tar czf ice-cfd-server-installer.tgz * --transform 's,^,ice-cfd-server-installer/,' 
